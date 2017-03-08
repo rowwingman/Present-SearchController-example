ced9eb8218d158d5c99958b8cb53a4cf26d2a960
Maksym Prokopchuk <rowwingman@gmail.com>
2017-03-08 15:51:48 +0200

    Add searchBar to collectionView

---
+++ SearchControllerExample/Tabs/CollectionView/SearcBarCollectionReusableView.swift
@@ -0,0 +1,13 @@
+//
+//  SearcBarCollectionReusableView.swift
+//  Rozetka
+//
+//  Created by Maksym Prokopchuk on 8/3/17.
+//  Copyright © 2017 OWOX. All rights reserved.
+//
+
+import UIKit
+
+class SearcBarCollectionReusableView: UICollectionReusableView {
+        
+}
--- SearchControllerExample/Tabs/CollectionView/SearchBarInCollectionViewController.swift
+++ SearchControllerExample/Tabs/CollectionView/SearchBarInCollectionViewController.swift
@@ -20,12 +20,99 @@ class SearchBarInCollectionViewController: UIViewController {
         return searchController
     }()
 
+    fileprivate let kCategoryCellIdentifier = "MainContentCategoryCollectionViewCellIdentifier"
+
+    lazy var collectionViewLayout: UICollectionViewFlowLayout = {
+        let layout = UICollectionViewFlowLayout()
+//        layout.minimumLineSpacing = 1.0
+//        layout.minimumInteritemSpacing = 1.0
+//        layout.sectionInset = UIEdgeInsets(top: 0.0, left: 0.0, bottom: 0.0, right: 0.0)
+        layout.itemSize = CGSize(width: 150.0, height: 150.0)
+//        layout.sectionInset = UIEdgeInsetsMake(16.0, 8.0, 16.0, 8.0)
+        return layout
+    }()
+
+    lazy var collectionView: UICollectionView = {
+        let collectionView = UICollectionView(frame: CGRect.zero, collectionViewLayout: self.collectionViewLayout)
+        collectionView.backgroundColor = UIColor.gray
+        collectionView.alwaysBounceVertical = true
+        collectionView.translatesAutoresizingMaskIntoConstraints = false
+        return collectionView
+    }()
+
+
     override func viewDidLoad() {
         super.viewDidLoad()
 
         self.title = "CollectionView"
 
-        self.view.backgroundColor = UIColor.white
+        self.view.backgroundColor = UIColor.gray
+
+
+        self.view.addSubview(self.collectionView)
+
+        let views = ["collectionView" : collectionView]
+        let formatString = "|[collectionView]|"
+
+        let constraints = NSLayoutConstraint.constraints(withVisualFormat: formatString, options:[.alignAllTop] , metrics: nil, views: views)
+        let verticalConstraints = NSLayoutConstraint.constraints(withVisualFormat: "V:|[collectionView]|", options:[.alignAllTop] , metrics: nil, views: views)
+
+        NSLayoutConstraint.activate(constraints)
+        NSLayoutConstraint.activate(verticalConstraints)
+
+
+
+        self.collectionView.register(UICollectionViewCell.self,
+                                     forCellWithReuseIdentifier: self.kCategoryCellIdentifier)
+
+        self.collectionView.register(SearcBarCollectionReusableView.self,
+                                     forSupplementaryViewOfKind: UICollectionElementKindSectionHeader,
+                                     withReuseIdentifier: "headerView")
+
+        self.collectionViewLayout.headerReferenceSize = CGSize(width: self.collectionView.bounds.width, height: 100.0)
+
+
+        self.collectionView.dataSource = self
+        self.collectionView.delegate = self
     }
 
+}
+
+//MARK: UICollectionViewDataSource
+extension SearchBarInCollectionViewController: UICollectionViewDataSource {
+
+    func numberOfSections(in collectionView: UICollectionView) -> Int {
+        return 1
+    }
+
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return 10
+    }
+
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: self.kCategoryCellIdentifier, for: indexPath)
+        return cell
+    }
+
+    // http://stackoverflow.com/questions/21731318/add-a-simple-uiview-as-header-of-uicollectionview
+    // http://stackoverflow.com/questions/24962282/cant-use-uisearchcontroller-with-uicollectionview
+    func collectionView(_ collectionView: UICollectionView, viewForSupplementaryElementOfKind kind: String, at indexPath: IndexPath) -> UICollectionReusableView {
+        let headerView = collectionView.dequeueReusableSupplementaryView(ofKind: UICollectionElementKindSectionHeader,
+                                                                         withReuseIdentifier: "headerView",
+                                                                         for: indexPath)
+        if headerView is SearcBarCollectionReusableView {
+            if self.searchController.searchBar.superview == nil {
+                headerView.addSubview(self.searchController.searchBar)
+            }
+            self.searchController.searchBar.sizeToFit()
+        }
+
+        return headerView
+    }
+
+}
+
+//MARK: UICollectionViewDelegateFlowLayout
+extension SearchBarInCollectionViewController: UICollectionViewDelegateFlowLayout {
+    
 }
